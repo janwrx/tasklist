2f0fcaedffa1d5155d90f83f5c010740d84c9834
janwrx <janzosa@gmail.com>
2018-10-25 16:04:18 -0700

    duedate

--- tasklist.rb
+++ tasklist.rb
@@ -22,13 +22,19 @@ end
 
 
 class Task_List
-  attr_accessor :list
+  attr_accessor :list, :time_sensitive, :all_tasks
   def initialize
     @list = []
+    @time_sensitive = []
+    @all_tasks = []
   end
 
   def add_task (task)
-    @list << task
+    if task.class == Due_Date_Task
+      @time_sensitive << task
+    else
+      @list << task
+    end
   end
 
   def get_completed
@@ -49,6 +55,42 @@ class Task_List
       end
     end
     return incomplete_arr.join(', ')
+  end
+
+  def get_due_today
+    due_today = []
+    todays_date = "#{Time.new.month}/#{Time.new.day}"
+    #loop through timesensitive tasks
+    @time_sensitive.each do |task|
+      # if due date is same as current date and incomplete
+      if (task.due_date == todays_date) && !task.status
+        # it's due today, so push into 'due today'
+        due_today << task
+      end
+    end
+    return due_today
+  end
+
+  def sortby_date
+    @time_sensitive.sort_by! {|task| task.due_date}
+  end
+
+  def sort_all
+    @all_tasks = sortby_date << @list
+    @all_tasks.flatten!
+  end
+
+end
+
+class Due_Date_Task < Task
+  attr_accessor :due_date
+  def initialize title, description, due_date
+    super(title, description)
+    @due_date = due_date
+  end
+
+  def print_tasks
+       "#{@title} task (#{@description}) is due on #{@due_date}"
   end
 
 end
--- tasklist_spec.rb
+++ tasklist_spec.rb
@@ -34,6 +34,9 @@ describe Task_List do
     @task_list = Task_List.new
     @task1 = Task.new("get money", "get money")
     @task2 = Task.new("study", "read books")
+
+    @task3 = Due_Date_Task.new("pay bills", "give money to Verizon", "10/25")
+    @task4 = Due_Date_Task.new("pay bills", "give money to Janice", "07/28")
   end
 
   it "can create a task_list" do
@@ -62,5 +65,62 @@ describe Task_List do
     @task_list.add_task(@task2)
     expect(@task_list.get_incomplete). to be_a String
   end
+
+  it "can add items with due dates to my task_list" do
+    expect(@task_list.time_sensitive).to be_a Array
+  end
+
+  # Story: As a developer with a TaskList, I can list all the not completed items that are due today.
+
+  it "can list all the incomplete tasks that are due today" do
+    expect(@task_list.get_due_today). to be_an Array
+  end
+
+  it "successfully pushes incomplete task due today into due_today array" do
+    @task_list.add_task(@task3)
+    expect(@task_list.time_sensitive.length).to be 1
+    expect(@task_list.get_due_today.length).to be 1
+  end
+
+  # Story: As a developer with a TaskList, I can list all the incomplete items in order of due date.
+
+  # be >= 10 # also applicable for >, <=, < be_within(0.01).of(28.35) # floating point
+
+  it "can list all incomplete items in order of due date" do
+    @task_list.add_task(@task3)
+    @task_list.add_task(@task4)
+    @task_list.sortby_date
+    expect(@task_list.time_sensitive[0].due_date).to be < (@task_list.time_sensitive[1].due_date)
+  end
+  # Story: As a developer with a TaskList with and without due dates, I can list all the not completed items in order of due date, and then the items without due dates.
+
+  it "can list incomplete items with dates by date and then dateless tasks" do
+    @task_list.add_task(@task3)
+    @task_list.add_task(@task4)
+    @task_list.add_task(@task1)
+    @task_list.add_task(@task2)
+    @task_list.sort_all
+    expect(@task_list.all_tasks.last). to be_an_instance_of Task
+    expect(@task_list.all_tasks.first). to be_an_instance_of Due_Date_Task
+  end 
+
+end
+
+
+describe Due_Date_Task do
+  before do
+    @duedatetask = Due_Date_Task.new("go to bank", "go to bank", "November 2nd, 2018")
+  end
+# Story: As a developer, I can create a DueDateTask, which is-a Task that has-a due date.
+# Hint: Use the built-in Ruby Date class
+it "can create a new due date task" do
+  expect{Due_Date_Task.new("shit", "fuck", "date")}.to_not raise_error
+
+end
+# Story: As a developer, I can print an item with a due date with labels and values.
+# Hint: When implementing to_s, use super to call to_s on the super class.
+it "can print tasks with due dates, labels, and values" do
+  expect(@duedatetask.print_tasks).to be_a String
+end
 
 end
