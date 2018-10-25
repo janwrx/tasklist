dbe15a88e6628cd8cf670d984ff99544b11ccd0b
janwrx <janzosa@gmail.com>
2018-10-25 16:12:57 -0700

    Anniversary Instructions

--- tasklist_spec.rb
+++ tasklist_spec.rb
@@ -102,7 +102,7 @@ describe Task_List do
     @task_list.sort_all
     expect(@task_list.all_tasks.last). to be_an_instance_of Task
     expect(@task_list.all_tasks.first). to be_an_instance_of Due_Date_Task
-  end 
+  end
 
 end
 
@@ -124,3 +124,15 @@ it "can print tasks with due dates, labels, and values" do
 end
 
 end
+
+
+# Story: As a developer, I can create a task for an anniversary.
+# Hint: An Anniversary has a month and a day.
+# Hint: An Anniversary is a special kind of DueDateTask where the due date changes depending on the current date (override the due_date method to return the next annivesary date).
+
+
+# Story: As a developer, I can print an item for an anniversary with field labels and values.
+#
+# Story: As a developer with a TaskList with and without due dates and yearly recurring dates, I can list all the not completed items in order of due date and yearly dates for the current month.
+#
+# Story: As a developer with a TaskList with a collection of items with and without due dates and yearly recurring dates, I can list all the not completed items in order of due date and yearly dates for the current month, then the items without due dates.
